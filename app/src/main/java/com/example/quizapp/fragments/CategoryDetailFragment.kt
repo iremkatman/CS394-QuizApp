83bed024c040399a897ee5ddec1f5f3ee6fe371d
Can Önder <147001133+canonder09@users.noreply.github.com>
2024-12-01 19:02:54 +0300

    CategoryDetailFragment updated with proper json files to animated_picture

--- app/src/main/java/com/example/quizapp/fragments/CategoryDetailFragment.kt
+++ app/src/main/java/com/example/quizapp/fragments/CategoryDetailFragment.kt
@@ -6,6 +6,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
+import com.example.quizapp.R
 import com.example.quizapp.databinding.FragmentCategoryDetailBinding
 
 class CategoryDetailFragment : Fragment() {
@@ -21,10 +22,21 @@ class CategoryDetailFragment : Fragment() {
 
         val categoryName = arguments?.getString("categoryName") ?: "Unknown Category"
         val categoryId = arguments?.getInt("categoryId") ?: 0
-
+        val lottieAnimationView = binding.lottieAnimation
 
         binding.tvCategoryTitle.text = categoryName
         binding.textViewCategoryId.visibility = View.GONE
+
+
+        when (categoryName) {
+            "Sports" -> lottieAnimationView.setAnimation(R.raw.sports) // sports.json
+             "Math" -> lottieAnimationView.setAnimation(R.raw.math)
+
+        }
+
+        lottieAnimationView.playAnimation()
+
+
 
 
         binding.btnEasy.setOnClickListener {
